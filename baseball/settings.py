5e42e3760a48ec7fbbae531eac3f60a5afbcea65
josieh <hollingsworth.josie@gmail.com>
2014-03-05 15:31:52 -0500

    change wsgi

--- baseball/settings.py
+++ baseball/settings.py
@@ -131,7 +131,7 @@ TEMPLATE_LOADERS = (
 #DATABASES['default'] =  dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
-#SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Allow all host headers
 #ALLOWED_HOSTS = ['*']
